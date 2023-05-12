5d4cd82d6828a8c2d3487db3cbc2f0513d50023a
Leonel Cabral <69341023+CBeRs22@users.noreply.github.com>
2023-05-11 22:28:55 -0300

    Create Desafio1.py

---
+++ Desafio1.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+competidorxs=[]
+for i in range(20):
+    print(f"Ingrese los datos de la persona{i+1}")
+    apelnomb=input("Apellido y Nombre: ")
+    psto=(i+1)
+    edad=int(input("Edad: "))
+    mins=int(input("Minutos: "))
+    segs=int(input("Segundos: "))
+    tmpo=f"{mins:02d}:{segs:02d}"
+    pais=input("País de origen: " )
+    compe=(psto,apelnomb,edad,tmpo,pais)
+competidorxs.append(compe)
+conx=sqlite3.connect("CROSSANDINO.db") # Path del archivo
+dats=conx.cursor()
+for compe in competidorxs:
+    dats.execute("INSERT INTO Categorias VALUES (?, ?, ?, ?, ?)", compe)
+conx.commit()
+conx.close()
+print("Listo!")
